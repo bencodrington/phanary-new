f69dc2d20b9d5b41f98b62ca665ccc6acbffac2f
Ben Codrington <benvenda@gmail.com>
2024-08-04 17:34:58 -0400

    Update production to use v2 search endpoint

--- src/services/database.ts
+++ src/services/database.ts
@@ -7,7 +7,7 @@ export async function fetchSearchResults(searchText: string): Promise<SearchResu
     return [];
   }
   // const response = await fetch(`http://localhost:5001/turbo-bard/us-central1/search?searchText=${searchText}`);
-  const response = await fetch(`https://us-central1-turbo-bard.cloudfunctions.net/search?searchText=${trimmedSearchText}`);
+  const response = await fetch(`https://us-central1-turbo-bard.cloudfunctions.net/search_v2?searchText=${trimmedSearchText}`);
   const results = await response.json();
   return results as SearchResult[];
 }
